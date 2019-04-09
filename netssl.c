6ea6661ee4f31bc4ba2f4ef43bc9860f0dc62676
IN <nompelis@nobelware.com>
2019-04-09 16:51:43 -0500

    Keep changing protocols, in an endless effort to use the one that does not have security issues, and this is to no end, for sure... IN 2019/04/09

--- netssl.c
+++ netssl.c
@@ -278,7 +278,10 @@ int inOSSL_CreateServer( struct inOSSL_data_s *p,
 
 // p->method = SSLv2_server_method();
 // p->method = SSLv3_server_method();
-   p->method = TLSv1_server_method();
+// p->method = TLSv1_server_method();
+// p->method = TLSv1_1_server_method();
+   p->method = TLSv1_2_server_method();
+// p->method = TLSv1_3_server_method();
    p->sslctx = SSL_CTX_new( p->method );
 
    if( p->sslctx == NULL ) {
@@ -492,7 +495,10 @@ int inOSSL_CreateClient( struct inOSSL_data_s *p, char *keyfile, char *certfile)
 
 // p->method = SSLv2_client_method();
 // p->method = SSLv3_client_method();
-   p->method = TLSv1_client_method();
+// p->method = TLSv1_client_method();
+// p->method = TLSv1_1_client_method();
+   p->method = TLSv1_2_client_method();
+// p->method = TLSv1_3_client_method();
    p->sslctx = SSL_CTX_new( p->method );
 
    if( p->sslctx == NULL ) {
